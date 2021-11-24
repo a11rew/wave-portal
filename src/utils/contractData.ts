4530677126ba8408a4b10a3162ab8934fbfe9685
Andrew Glago <andrewglago1@gmail.com>
2021-11-24 14:48:37 +0000

    update contract data

---
+++ src/utils/contractData.ts
@@ -0,0 +1,8 @@
+import abi from "./WavePortal.json";
+
+// General contract info
+
+const contractAddress = "0xB1e48967c81BCaEECfa67cf0409A9b3C73b77c8C";
+const contractABI = abi.abi;
+
+export { contractAddress, contractABI };
