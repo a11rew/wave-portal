addf9dabd1926a77af3508a21dbe1fed769ce1ad
Andrew Glago <andrewglago1@gmail.com>
2021-11-24 10:27:09 +0000

    inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      ...colors,
+    },
   },
   variants: {
     extend: {},
